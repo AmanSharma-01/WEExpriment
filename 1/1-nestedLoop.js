b3b94c275619d43b6179164ccf4fed4b3f299758
Aman Sharma <amansharma10221@gmail.com>
2021-06-14 20:54:51 +0530

    more praticls added

--- 1/1-nestedLoop.js
+++ 1/1-nestedLoop.js
@@ -1,9 +1,9 @@
 // Using javascript demonstrate Nested loop.
 
-arr = [['A', 'B'], ['C', 'D', 'E', 'F']]
+arr = [['hi', 'this' , 'is', 'a', '2D', 'array'], ['C', 'D', 'E', 'F']]
 
 for (i = 0; i < arr.length; i++) {
     for (j = 0; j < arr[i].length; j++) {
-        console.log(arr[i][j])
+        console.log(arr[i][j]);
     }
 }
